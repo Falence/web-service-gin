760c07e187899c7d566582bebce5c4dc4ffb881d
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-14 21:00:32 +0100

    implement handler to get item by id

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-
+	
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,8 +15,8 @@ type album struct {
 }
 
 // album slice to seed record album data
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+var albums = []album {
+	{"1", "Blue Train", "John Coltrane",  56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
@@ -24,6 +24,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -46,4 +47,16 @@ func postAlbums(c *gin.Context) {
 	// add the new album to the slice
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, album := range albums {
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		} 
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Oops! Album not found"})
 }
